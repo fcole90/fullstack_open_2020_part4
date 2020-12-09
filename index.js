234b103530a224a2a546406858bb75332cc22b37
Fabio Colella <fcole90@gmail.com>
2020-12-09 12:29:36 +0200

    Separed blog model

--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config();
+const Blog = require("./models/blog")
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
---
+++ models/blog.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+module.exports = Blog
